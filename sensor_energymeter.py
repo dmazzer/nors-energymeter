ef9b6de7fd922fad2f2356e8937a31f0a56370ae
daniel mazzer <dmazzer@gmail.com>
2017-08-25 11:26:36 -0300

    try to reconnect the serial port if stop receiving data

--- sensor_energymeter.py
+++ sensor_energymeter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-""" 
+"""
 sensor_power.py: Current and Voltage sensor driver
 
 This sensor driver collects current and voltage measurements from external arduino board.
@@ -17,11 +17,8 @@ import signal
 import serial
 
 from genericsensor.genericsensor import Nors_GenericSensor
-
 from grovepi import *
-
 from norsutils.logmsgs.logger import Logger
-
 from serial_ports import serial_ports
 
 logger = Logger('debug')
@@ -32,35 +29,47 @@ class RealSensor(Nors_GenericSensor):
 
         self.sensor_name = 'PWRMTR'
         self.message = {}
-        
+
+        self.no_data_timeout_reset = 5;
+        self.no_data_timeout = no_data_timeout_reset;
+
         if self.connect_to_energy_meter() == True:
             super(RealSensor, self).__init__(
                                              gs_name = self.sensor_name,
                                              gs_id = 'bdeb5806-a9a2-11e6-a119-080027bc22fd',
-                                             gs_description = 'Dual phase power meter', 
+                                             gs_description = 'Dual phase power meter',
                                              gs_interface = None,
                                              gs_read_interval = 60)
         else:
             logger.log('Energy meter device was not found on serial ports', 'error')
             raise SystemExit('Exiting')
 
-        
+
     def SensorRead(self):
         if self.ser.inWaiting() > 0 :
             msg = self.ser.readline()
             measures = self.msg_parse(msg)
-#             if measures is None:
-#                 self.ser.flushInput()
-#                 self.ser.flushOutput()
-                
             logger.log(str(measures), 'debug')
+            self.no_data_timeout = self.no_data_timeout_reset
             return measures
         else:
+            if self.no_data_timeout > 0:
+                self.no_data_timeout = self.no_data_timeout - 1
+            else:
+                logger.log('Energy meter device is not sending data... will try to reconnect', 'info')
+                self.no_data_timeout = self.no_data_timeout_reset
+                self.reset_serial_port()
+                res = self.connect_to_energy_meter()
+                if res == True:
+                    logger.log('Energy meter device is (re)connected', 'info')
+                else:
+                    logger.log('Energy meter device fail to reconnect', 'error')
+
             return None
 
     def SensorDataProcessing(self,sensor_data):
         return sensor_data
-    
+
     def connect_to_energy_meter(self):
         logger.log('Searching serial ports','info')
         ports = serial_ports()
@@ -74,66 +83,65 @@ class RealSensor(Nors_GenericSensor):
                     self.ser.flushInput()
                     self.ser.flushOutput()
                     self.ser.write('#')
-#                     time.sleep(2)
                     check_em = self.ser.read()
                     if check_em == '!':
                         logger.log('Success on port ' + str(port),'info')
                         return True
                     retry = retry-1
-#                     time.sleep(2)
-                
+
             except (serial.SerialException) as e:
                 logger.log('Energy meter device serial port exception', 'error')
+                self.ser.close()
                 print(e)
                 return False
-        
+
         return False
+
+    def reset_serial_port(self):
+        try:
+            self.ser.close()
+        except (serial.SerialException) as e:
+            print(e)
 
     def msg_parse(self, msg):
         msg_p = msg.split()
-        
+
         # checking message parsing result using list size
         if len(msg_p) != 6:
             return None
-        
+
         measure = {}
         # for python 3 use:
         # measure['channel'] = int(str(msg_p[0]).replace("b'@", "").replace("'", ""))
         channel = int(str(msg_p[0]).replace("@", ""))
         if (channel != 1) and (channel != 2):
             return None
-        
+
         measure['channel'] = channel
         measure['real_power'] = float(msg_p[1])
-        measure['apparent_power'] = float(msg_p[2]) 
-        measure['Vrms'] = float(msg_p[3]) 
-        measure['Irms'] = float(msg_p[4]) 
+        measure['apparent_power'] = float(msg_p[2])
+        measure['Vrms'] = float(msg_p[3])
+        measure['Irms'] = float(msg_p[4])
         measure['power_factor'] = float(msg_p[5])
-#         return {str(channel): measure}
-        
-        return self.msg_agregate(measure) 
+
+        return self.msg_agregate(measure)
 
     def msg_agregate(self, msg):
-#         print(msg)
         if msg['channel'] == 1:
             self.message = {'ch1': msg}
-#             print('canal1')
             return None
         if msg['channel'] == 2:
             self.message['ch2'] = msg
-#             print('canal2')
             return self.message
-        
+
 if __name__ == '__main__':
     sensor = RealSensor()
 
 
     def do_exit(sig, stack):
         raise SystemExit('Exiting')
-    
+
     signal.signal(signal.SIGINT, do_exit)
     signal.signal(signal.SIGUSR1, do_exit)
-    
-    signal.pause()    
 
-    
+    signal.pause()
